86fbdc374eb95469392d45fa03f13e52384f26ba
Hoon <87412862+BitAdventurer@users.noreply.github.com>
2023-09-30 02:24:44 +0900

    Update seed.py

--- seed.py
+++ seed.py
@@ -2,11 +2,12 @@ import os
 import numpy as np
 import torch
 import random
-def seed_everything(seed=100):
+
+def seed_everything(seed=42):  # Changed default seed to 42, often used as default in ML
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
-    torch.manual_seed(100)
-    torch.cuda.manual_seed(100)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False  # Set to False for more deterministic behavior

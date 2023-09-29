10436100c0f635f3ec07f8f87a922ea4afd271bc
Hoon <87412862+BitAdventurer@users.noreply.github.com>
2023-09-30 03:43:55 +0900

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,6 @@ parser.add_argument("--action", default=117, type=int, help="N of action")
 parser.add_argument("--reward", default=1.0, type=float, help="Reward")
 parser.add_argument('--seed', default=, type=int, help='random seed')
 parser.add_argument('--num_epoch', default=, type=int, help='num_epoch')
-parser.add_argument('--one_hot', default=False, type=str, help='One_hot encoding')
 
 # training hyperparams
 parser.add_argument('--lr', default=0.005, type=float, help='learning rate')
@@ -53,34 +52,34 @@ parser.add_argument("--patience", default=, type=int, help="Early stop point")
 parser.add_argument("--window", default=, type=int, help="Sampling window size")
 
 # actor-critic
-parser.add_argument("--actor_layer1", default=512, type=int, help="Actor network layer1")
-parser.add_argument("--actor_layer2", default=256, type=int, help="Actor network layer2")
-parser.add_argument("--actor_layer", default=117, type=int, help="Actor network layer6")
+parser.add_argument("--actor_layer1", default=, type=int, help="Actor network layer1")
+parser.add_argument("--actor_layer2", default=, type=int, help="Actor network layer2")
+parser.add_argument("--actor_layer", default=, type=int, help="Actor network layer6")
 
-parser.add_argument("--critic_layer1", default=64, type=int, help="Critic network layer1")
-parser.add_argument("--critic_layer2", default=32, type=int, help="Critic network layer2")
-parser.add_argument("--critic_layer", default=1, type=int, help="Critic network layer6")
+parser.add_argument("--critic_layer1", default=, type=int, help="Critic network layer1")
+parser.add_argument("--critic_layer2", default=, type=int, help="Critic network layer2")
+parser.add_argument("--critic_layer", default=, type=int, help="Critic network layer6")
 
-parser.add_argument("--temp_layer1", default=64, type=int, help="Actor network layer1")
-parser.add_argument("--temp_layer2", default=32, type=int, help="Actor network layer2")
-parser.add_argument("--temp_layer", default=1, type=int, help="Actor network layer6")
+parser.add_argument("--temp_layer1", default=, type=int, help="Actor network layer1")
+parser.add_argument("--temp_layer2", default=, type=int, help="Actor network layer2")
+parser.add_argument("--temp_layer", default=, type=int, help="Actor network layer6")
 
-parser.add_argument("--action_emb_layer", default=32, type=int, help="Action emb layer")
-parser.add_argument("--state_emb_layer", default=32, type=int, help="State emb layer")
+parser.add_argument("--action_emb_layer", default=, type=int, help="Action emb layer")
+parser.add_argument("--state_emb_layer", default=, type=int, help="State emb layer")
 
 # buffer
 parser.add_argument("--self_data", default=path+'/self_play_best_data', type=str, help="Generated data")
 parser.add_argument("--replay_buffer", default=True, type=bool, help="Replay buffer use")
 parser.add_argument("--buffer_data", default=path+'/self_play_backup', type=str, help="Generated data path")
-parser.add_argument("--buffer_size", default=1000, type=int, help="Buffer_size")
-parser.add_argument("--sampling_size", default=2, type=int, help="Sampling_size")
+parser.add_argument("--buffer_size", default=, type=int, help="Buffer_size")
+parser.add_argument("--sampling_size", default=, type=int, help="Sampling_size")
 
 # tagerget network
 parser.add_argument("--target_network", default=True, type=bool, help="Used target network ")
 parser.add_argument("--soft_update", default=False, type=bool, help="Soft update")
-parser.add_argument("--soft_update_ratio", default=0.01, type=float, help="Soft update ratio")
-parser.add_argument("--target_n", default=1, type=int, help="Target network freeze N")
-parser.add_argument("--target_soft_update_ratio", default=0.01, type=float, help="Target soft update ratio")
+parser.add_argument("--soft_update_ratio", default=, type=float, help="Soft update ratio")
+parser.add_argument("--target_n", default=, type=int, help="Target network freeze N")
+parser.add_argument("--target_soft_update_ratio", default=, type=float, help="Target soft update ratio")
 
 # data
 fold = 0 #fold1-5
@@ -92,21 +91,17 @@ if fold == 0:
 
 
 parser.add_argument("--n_split", default=5, type=int, help="N")
-parser.add_argument("--jeilt", default=0.0, type=float, help="Threshold")
 
 # device
 parser.add_argument("--cuda_device", default=0, type=int, help="Cuda")
 
 # Clamp
 parser.add_argument("--w_clamp", default=False, type=bool, help="Weight clamp")
-parser.add_argument("--w_clamp_v", default=1, type=float, help="Weight clamp value")
+parser.add_argument("--w_clamp_v", default=, type=float, help="Weight clamp value")
 parser.add_argument("--g_clamp", default=False, type=bool, help="Grandient clamp")
-parser.add_argument("--g_clamp_v", default=2, type=float, help="Grandient clamp value")
+parser.add_argument("--g_clamp_v", default=, type=float, help="Grandient clamp value")
 
 # Plot
 parser.add_argument("--eval", default=True, type=bool, help="evaluate")
 parser.add_argument("--val_eval", default=False, type=bool, help="evaluate")
 parser.add_argument("--feature", default=True, type=bool, help="Critic feature")
-
-# test
-parser.add_argument("--state_value", default=True, type=bool, help="State value")

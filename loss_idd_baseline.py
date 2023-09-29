cc8c0a07b7ce1db72ba344cb9c0bd0c197f4f7a2
Hoon <87412862+BitAdventurer@users.noreply.github.com>
2023-09-30 02:23:56 +0900

    Update loss_idd_baseline.py

--- loss_idd_baseline.py
+++ loss_idd_baseline.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import config
-args = config.parser.parse_args()
 
-path=args.path
+args = config.parser.parse_args()
+path = args.path
 
 
 def loss_idd(i):
-    data = pd.read_csv(path+f'/baseline/fold{args.fold}/baseline_loss{i}.csv')
+    data = pd.read_csv(f'{path}/baseline/fold{args.fold}/baseline_loss{i}.csv')
 
-    plt.plot(data['0'], label='train',marker='o', ls='-', markersize=2)
-    plt.plot(data['0.1'], label='val',marker='o', ls='-', markersize=2)
-    plt.plot(data['0.2'], label='test',marker='o', ls='-', markersize=2)
+    plt.plot(data['0'], label='train', marker='o', ls='-', markersize=2)
+    plt.plot(data['0.1'], label='val', marker='o', ls='-', markersize=2)
+    plt.plot(data['0.2'], label='test', marker='o', ls='-', markersize=2)
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
     plt.tight_layout()
-    plt.savefig(path+f'/baseline/fold{args.fold}/loss.png', format='png', dpi=300)
-    plt.close()
+    plt.savefig(f'{path}/baseline/fold{args.fold}/loss.png', format='png', dpi=300)
+    plt.show()  # Moved here to actually show the plot
+
+
 if __name__ == "__main__":
-    loss_idd()
-    plt.show()
+    # For demonstration purposes, assuming `i` is 1. Replace with appropriate value or loop over multiple values.
+    loss_idd(1)

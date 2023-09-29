32c7641127a4c7d8397f3d9c2a168a363b8357f1
Hoon <87412862+BitAdventurer@users.noreply.github.com>
2023-09-30 02:21:18 +0900

    Update Chevconv.py

--- Chevconv.py
+++ Chevconv.py
@@ -4,27 +4,27 @@ import math
 import gcn_util
 
 def chev_conv(self, x, adj_orig):
-    # A` * X * W
-    torch.cuda.empty_cache()
-    x = x.unsqueeze(0) if x.dim() == 2 else x
-    adj = gcn_util.laplacian_norm_adj(adj_orig)
-    adj = gcn_util.add_self_loop(-adj)
+    torch.cuda.empty_cache()  # Clearing CUDA cache, if necessary
+    x = x.unsqueeze(0) if x.dim() == 2 else x  # Ensure the input has three dimensions
+    
+    adj = gcn_util.laplacian_norm_adj(adj_orig)  # Compute normalized Laplacian
+    adj = gcn_util.add_self_loop(-adj)  # Add self-loop to adjacency matrix
     Tx_0 = x[0]
-    Tx_1 = x[0]  # Dummy.
-
+    
+    # Perform the convolution
     out = torch.matmul(Tx_0, self.weight[0])
-    # propagate_type: (x: Tensor, norm: Tensor)
-
-    if self.weight.shape[0] > 1: 
-        Tx_1 = torch.matmul(adj, x)
-        out = out + torch.matmul(Tx_1, self.weight[1])
-        
+    
+    if self.weight.shape[0] > 1:  # If more than one weight matrix is present
+        Tx_1 = torch.matmul(adj, x)  # Compute the first transformation
+        out += torch.matmul(Tx_1, self.weight[1])  # Update the output
+    
+    # Compute the subsequent transformations if more weight matrices are present
     for k in range(2, self.weight.shape[0]):
-        Tx_2 = torch.matmul(adj, Tx_1)
-        Tx_2 = 2. * Tx_2 - Tx_0
-        out = out + torch.matmul(Tx_1, self.weight[k])
-        Tx_0, Tx_1 = Tx_1, Tx_2
+        Tx_2 = 2 * torch.matmul(adj, Tx_1) - Tx_0
+        out += torch.matmul(Tx_1, self.weight[k])
+        Tx_0, Tx_1 = Tx_1, Tx_2  # Update Tx_0 and Tx_1 for the next iteration
+    
     if self.bias is not None:
-        out += self.bias
-
+        out += self.bias  # Add the bias if it is not None
+    
     return out

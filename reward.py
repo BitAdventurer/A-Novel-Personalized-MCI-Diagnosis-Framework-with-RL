1597078c49526d0a2018f081204557d0accac0bb
Hoon <87412862+BitAdventurer@users.noreply.github.com>
2024-04-23 13:41:19 +0900

    Update reward.py

--- reward.py
+++ reward.py
@@ -1,47 +1,69 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
-import util
-import config
-import numpy as np
 import ast
-import wandb
+
+import config
 args = config.parser.parse_args()
 path = args.path
+
+def read_data(file_path):
+    """
+    Helper function to read and parse data from a given filepath.
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = [ast.literal_eval(line.strip()) for line in file if line.strip()]
+        return data
+    except FileNotFoundError:
+        print(f"Error: File not found {file_path}")
+        return []
+    except SyntaxError:
+        print(f"Error: Syntax error in the data in the file {file_path}")
+        return []
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return []
+
+def plot_metrics(metrics, labels, colors, linestyle, title, xlabel, ylabel, save_path):
+    """
+    Function to plot given metrics with specified attributes.
+    """
+    plt.figure(figsize=(20, 8))
+    plt.title(title)
+    
+    for metric, label, color, line in zip(metrics, labels, colors, linestyle):
+        plt.plot(metric, label=label, marker='o', linestyle=line, linewidth=1.5, markersize=1, color=color)
+
+    plt.axhline(0, color='green', linestyle='-')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(save_path, format='jpg', dpi=300)
+    plt.close()
 
 def plot_acc_val():
     """
     Function to plot different accuracy metrics such as accuracy, sensitivity, specificity, and F1.
     """
+    metrics_files = [
+        '/result/confusion_info/val_acc_plot_q.txt',
+        '/result/confusion_info/val_sensitivity_plot_q.txt',
+        '/result/confusion_info/val_specificy_plot_q.txt',
+        '/result/confusion_info/val_f1_plot_q.txt'
+    ]
+    labels = ['Accuracy', 'Sensitivity', 'Specificity', 'F1 Score']
+    colors = ['red', '#ff7f0e', 'blue', 'black']
+    linestyles = ['-', '-', '-', '--']
     
-    # Read and process accuracy data
-    with open(path + '/result/confusion_info/val_acc_plot_q.txt', 'r', encoding='utf-8') as f:
-        total_acc_q = [ast.literal_eval(line) for line in f.readlines()]
-
-    # Read and process sensitivity data
-    with open(path + '/result/confusion_info/val_sensitivity_plot_q.txt', 'r', encoding='utf-8') as f:
-        sensitivity_q = [ast.literal_eval(line) for line in f.readlines()]
-
-    # Read and process specificity data
-    with open(path + '/result/confusion_info/val_specificy_plot_q.txt', 'r', encoding='utf-8') as f:
-        specificity_q = [ast.literal_eval(line) for line in f.readlines()]
-
-    # Read and process F1 data
-    with open(path + '/result/confusion_info/val_f1_plot_q.txt', 'r', encoding='utf-8') as f:
-        f1_q = [ast.literal_eval(line) for line in f.readlines()]
+    metrics = [read_data(path + file) for file in metrics_files]
     
-    # Plotting
-    plt.figure(figsize=(20, 8))
-    plt.title('Validation Accuracy Metrics')
-    plt.plot(total_acc_q, label='Accuracy', marker='o', linestyle='-', linewidth=1.5, markersize=1, color='red')
-    plt.plot(sensitivity_q, label='Sensitivity', marker='o', linestyle='-', linewidth=0.5, markersize=1, color='#ff7f0e')
-    plt.plot(specificity_q, label='Specificity', marker='o', linestyle='-', linewidth=0.5, markersize=1, color='blue')
-    plt.plot(f1_q, label='F1 Score', marker='o', linestyle='--', linewidth=0.5, markersize=1, color='black')
-    plt.axhline(0, color='green', linestyle='-')
-    plt.xlabel('Iteration')
-    plt.ylabel('Delta')
-    plt.legend()
-    plt.grid()
-    plt.tight_layout()
-    plt.savefig(path + '/result/val_acc.jpg', format='jpg', dpi=300)
-    plt.close()
+    plot_metrics(
+        metrics, labels, colors, linestyles,
+        'Validation Accuracy Metrics', 'Iteration', 'Delta',
+        path + '/result/val_acc.jpg'
+    )
+
+if __name__ == "__main__":
+    plot_acc_val()
